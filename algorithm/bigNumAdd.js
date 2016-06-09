2bb121d3db65d43ce2742507282df8a4c2dcb915
haili042 <haili042@163.com>
2016-06-09 20:57:46 +0800

    大数相加算法

---
+++ algorithm/bigNumAdd.js
@@ -0,0 +1,36 @@
+/*
+* 第二题大树相加
+* */
+
+var sum = function(num1, num2) {
+    var arr1 = (num1 + '').split(''),
+        arr2 = (num2 + '').split(''),
+        len1 = arr1.length - 1,
+        len2 = arr2.length - 1,
+        index = Math.max(len2, len2) + 1,
+        res = [],
+        addUp = 0
+        ;
+
+
+    while(index > -1) {
+        var s = parseInt(arr1[len1--] || 0) + parseInt(arr2[len2--] || 0) + addUp;
+        res[index] = s;
+
+        addUp = 0;
+        if (s >= 10) {
+            addUp++;
+            res[index] = s % 10;
+        } else {
+            res[index] = s;
+        }
+
+        index--;
+    }
+
+    !res[0] && res.shift();
+
+    return res.join('');
+};
+
+console.log(sum(868683838345436, 3135363613631316));
