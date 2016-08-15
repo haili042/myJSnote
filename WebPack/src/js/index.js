7184e44c4cfcf014e4c1c5131ea2451a77519d7a
haili042 <haili042@163.com>
2016-08-15 10:06:49 +0800

    webpack 的学习

--- WebPack/src/js/index.js
+++ WebPack/src/js/index.js
@@ -1,8 +1,13 @@
 
 require('css/test.css');
 
+require('less/test');
+
+
 var a = require('js/cycleA');
 
 var cbx = require('js/cbx');
+
+var dg = require('js/dg');
 
 console.log(a);
--- webpack.build.config.js
+++ webpack.build.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 var cssExporter = new ExtractTextWebpackPlugin('[name].css');
-var lessExporter = new ExtractTextWebpackPlugin('less.css');
+var lessExporter = new ExtractTextWebpackPlugin('[name].css');
 
 module.exports = {
 
@@ -16,6 +16,7 @@ module.exports = {
         }),
 
         // 将公共代码抽离出来合并为一个文件插件
+        //new webpack.optimize.CommonsChunkPlugin('libs', 'libs.js'),
         new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
 
 
@@ -31,6 +32,7 @@ module.exports = {
 
         // 添加要打包在 common 里面的库
         'common' : ['jquery', 'easyui'],
+        //'common' : ['easyui'],
     },
 
     //入口文件输出配置
@@ -44,8 +46,7 @@ module.exports = {
         //加载器配置
         loaders: [
             { test: /\.css$/, loader: cssExporter.extract('style-loader', 'css-loader') },
-            { test: /\.less$/, loader: lessExporter.extract('style-loader', 'css-loader', 'less?sourceMap')},
-            { test: /\.jade$/, loader: "jade-loader" },
+            { test: /\.less$/, loader: 'style!css!less'},
             { test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192'}
         ]
     },
@@ -60,27 +61,17 @@ module.exports = {
         //查找module的话从这里开始查找
         // 路径一定要这么写.....不然一大波错误....................................
         root: path.join(__dirname, 'WebPack/src'),
-        //root: path.resolve('WebPack/src'),
 
         //自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
-        extensions: ['', '.js', 'css', '.json', '.less', '.jade'],
+        extensions: ['', '.js', '.json', '.less'],
 
         // 配置别名
         alias: {
-            'jquery' : path.join(__dirname, 'WebPack/lib/jquery.min.js'),
-            'easyui' : path.join(__dirname, 'WebPack/lib/easyui-1.4.3/jquery.easyui.min.js'),
-            'easyuiCss' : path.join(__dirname, 'WebPack/lib/easyui-1.4.3/easyui.css'),
+            jquery : path.join(__dirname, 'WebPack/lib/jquery.min.js'),
+            easyui : path.join(__dirname, 'WebPack/lib/easyui-1.4.3/jquery.easyui.min.js'),
+            easyuiCss : path.join(__dirname, 'WebPack/lib/easyui-1.4.3/easyui.css'),
         }
     },
 
-    // 不包含列信息，同时 loader 的 sourcemap 也被简化为只包含对应行的。最终的 sourcemap 只有一份，它是 webpack 对 loader 生成的 sourcemap 进行简化，然后再次生成的。
-    devtool: 'cheap-module-eval-source-map',
-
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
-    },
 };
 
